625b6f07d8baab549a43960f4b5dfecb1928978b
Pavel-personal <62989423+Pavel-personal@users.noreply.github.com>
2020-05-21 23:48:38 +0300

    site#3
    amazing trip

---
+++ amazing_tours/scripts.js
@@ -0,0 +1,13 @@
+// burger handler
+
+const selectElement = (s) => document.querySelector(s);
+
+//  open the menu on click
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+//  close the menu on click
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
